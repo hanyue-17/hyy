e5f410215519d38c525e1328bbbcd3143c6fde7e
Han <947768848@qq.com>
2020-04-07 14:11:51 +0800

    显示home组件

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,16 +1,14 @@
 <template>
-	<div>
-		<el-form label-position="top" label-width="80px" >
-			<el-form-item label="名称">
-				姓名
-			    <el-input v-model="formLabelAlign.name"></el-input>
+	<div class="login-wrap">
+		<el-form class="login-from" label-position="top" label-width="80px" :model="formdata">
+			<h2>用户登录</h2>
+			<el-form-item label="用户名">
+			    <el-input v-model="formdata.username"></el-input>
 			</el-form-item>
-			<el-form-item label="活动区域">
-			    <el-input v-model="formLabelAlign.region"></el-input>
+			<el-form-item label="密码">
+			    <el-input v-model="formdata.password"></el-input>
 			</el-form-item>
-			<el-form-item label="活动形式">
-			    <el-input v-model="formLabelAlign.type"></el-input>
-			</el-form-item>
+			<el-button @click.prevent="handleLogin()" class="login-btn" type="primary">登录</el-button>
 		</el-form>
 	</div>
 </template>
@@ -19,11 +17,43 @@
 export default{
 	data(){
 		return {
-	        
+	        formdata: {
+		        username: '',
+		        password: ''
+	        }
 	    }
+	},
+	methods:{
+		//登录请求
+		handleLogin(){
+			//接口文件上写的，请求路径：login，请求参数，username,password
+			this.axios.post('login',this.formdata)
+				.then((res)=>{
+					const{
+						data,
+						meta:{msg,status}
+					} = res.data
+					
+					//判断
+					//如果提交成功
+					if(status===200){
+						this.$message.success(msg);
+						this.$router.push({name:"home"})
+					}else{
+						//如果提交失败
+						this.$message.error(msg);
+					}
+					
+					
+				})
+		}
 	}
 }
 </script>
 
 <style>
+h2{font-size: 18px; font-weight: bold;}
+.login-wrap{width: 100%;height: 100%; background: #2b4b6b;}
+.login-from{ width: 450px; height: 300px; background: #fff; padding: 20px; border-radius: 10px;position:fixed;margin:auto;left:0; right:0; top:0; bottom:0; }
+.login-btn{width: 100%;}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,12 +4,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Reset from '@/assets/css/reset.css'
 
 //ui框架
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
+
+//axios
+import axios from 'axios'
+Vue.prototype.axios = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
 
 //如果变成生产模式，自动关闭一些console输出
 Vue.config.productionTip = false
